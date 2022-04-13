1aa646741dddc214e498f4058529dde2890ff746
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-04-13 23:20:39 +0900

    id value OK

---
+++ index.js
@@ -0,0 +1,6 @@
+const register = () => {
+  // input要素のtype属性を取得
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+  console.log(email, password);
+};
